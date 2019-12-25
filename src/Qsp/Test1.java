0ffed2c5b78d1a8b48a7452d37130b59a6d32e49
pra-us <57428274+pra-us@users.noreply.github.com>
2019-12-25 12:15:48 +0530

    Update Test1.java

--- src/Qsp/Test1.java
+++ src/Qsp/Test1.java
@@ -5,7 +5,7 @@ public class Test1 {
 	public static void main(String[] args) {
 		System.out.println("hi");
 		
-		System.out.println("hello");
+		System.out.println("hi hello");
 		
 		System.out.println("bye");
 		System.out.println("bye");
